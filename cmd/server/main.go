892c9c1d3f024005af7b40f4b4d95164df236472
kont1n <user@MacBook-Pro.local>
2024-06-16 12:01:48 +0300

    test 1

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,20 @@
 package main
 
-func main() {}
+import (
+	"github.com/kont1n/MetricsCollection/internal/receive"
+	"github.com/kont1n/MetricsCollection/internal/storage"
+	"net/http"
+)
+
+func main() {
+
+	memStorage := storage.CreateLocalStorage()
+
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/update/{metricType}/{metricName}/{metricValue}`, receive.UpdateHandler(memStorage))
+
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ internal/receive/updateHandler.go
@@ -0,0 +1,49 @@
+package receive
+
+import (
+	"github.com/kont1n/MetricsCollection/internal/storage"
+	"net/http"
+	"strconv"
+)
+
+const (
+	ContentTypeText = "text/plain"
+)
+
+func UpdateHandler(s storage.MetricStorage) func(http.ResponseWriter, *http.Request) {
+	return func(res http.ResponseWriter, req *http.Request) {
+
+		if req.Method != http.MethodPost {
+			http.Error(res, "Only POST requests are allowed!", http.StatusMethodNotAllowed)
+			return
+		}
+
+		metricType := req.PathValue("metricType")
+		metricName := req.PathValue("metricName")
+		switch metricType {
+		case "gauge":
+			{
+				if value, err := strconv.ParseFloat(req.PathValue("metricValue"), 64); err == nil {
+					s.ValueReplace(metricType, metricName, value)
+				} else {
+					http.Error(res, err.Error(), http.StatusBadRequest)
+				}
+			}
+		case "counter":
+			{
+				if value, err := strconv.ParseInt(req.PathValue("metricValue"), 10, 64); err == nil {
+					s.ValueIncrement(metricType, metricName, value)
+				} else {
+					http.Error(res, err.Error(), http.StatusBadRequest)
+				}
+			}
+		default:
+			{
+				http.Error(res, "Metric type not found!", http.StatusBadRequest)
+				return
+			}
+		}
+		res.Header().Set("Content-Type", ContentTypeText)
+		res.WriteHeader(http.StatusOK)
+	}
+}
